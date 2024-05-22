8628bf2645e026cd9d0b7178cb6a08d5fa33c8c6
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2024-05-22 17:37:32 +0800

    test: 嘗試使 Actions 显示错误

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -387,8 +387,9 @@ async def main():
         "nonebot_plugin_mystool",
         "",
     )
-    await test.run()
+    return await test.run()
 
 
 if __name__ == "__main__":
-    run(main())
+    is_success, _ = run(main())
+    exit(is_success)
