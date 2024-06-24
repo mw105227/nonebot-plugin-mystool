b605bb1f51b830a547148df7604ec613a89fe8dc
joseandluue <joseluue@outlook.com>
2024-06-24 09:19:36 +0800

    删除注释代码

--- src/nonebot_plugin_mystool/api/weibo.py
+++ src/nonebot_plugin_mystool/api/weibo.py
@@ -257,15 +257,12 @@ class WeiboSign:
                         res = await client.get(url, headers=headers, params=params_copy, timeout=10)
                     res_data = json.loads(res.text)
                     logger.info(f'微博签到返回：{res_data}')
-                    # if 'msg' in res_data and 'errmsg' not in res_data:      # 今日首次签到成功                
-                    if str(res_data.get('result')) == '1':
+                    if str(res_data.get('result')) == '1':                    # 今日首次签到成功      
                         msg += f"{ch['title_sub']}  ✅\n"
-                    # elif 'errmsg' in res_data :                             # 签到出错
-                    else:
-                        # msg = f"{res_data['errmsg']}\n"
+                    else:                                                     # 签到出错
                         msg += f"{ch['title_sub']}  ❌\n"
                         msg += f"--{res_data['errmsg'] if res_data.get('errmsg') else res_data['msg']}\n"
-                elif ch['is_sign'] == '已签':                               # 今日再次进行签到，且之前已经签到成功
+                elif ch['is_sign'] == '已签':                                 # 今日再次进行签到，且之前已经签到成功
                     msg += f"{ch['title_sub']}  ✅\n"
             return msg
         except Exception as e:
